f6e698da37ce2913b7da993cf09a4fd245f0bb98
Tim-Louis Rieck <timlouis.rieck@gmail.com>
2025-04-02 07:51:13 +0200

    fixing stuff

--- mobile/multichoiceset.js
+++ mobile/multichoiceset.js
@@ -20,15 +20,15 @@
  * @copyright  2024 Tim-Louis Rieck <tim-louis.rieck@oncampus.de>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define("qtype_multichoiceset/mobile", ["core/str"], (str) => {
-	var module = {
+define(["core/str"], (str) => {
+	const module = {
 		componentInit: function () {
 			if (!this.question) {
 				return this.CoreQuestionHelperProvider.showComponentError(this.onAbort);
 			}
 
 			// Create a temporary div to ease extraction of parts of the provided html.
-			var div = document.createElement("div");
+			const div = document.createElement("div");
 			div.innerHTML = this.question.html;
 
 			// Replace Moodle's correct/incorrect classes, feedback and icons with mobile versions.
@@ -36,9 +36,9 @@ define("qtype_multichoiceset/mobile", ["core/str"], (str) => {
 			this.CoreQuestionHelperProvider.replaceFeedbackClasses(div);
 
 			// Get useful parts of the provided question html data.
-			var questiontext = div.querySelector(".qtext");
-			var prompt = div.querySelector(".prompt");
-			var answeroptions = div.querySelector(".answer");
+			const questiontext = div.querySelector(".qtext");
+			const prompt = div.querySelector(".prompt");
+			const answeroptions = div.querySelector(".answer");
 
 			// Add the useful parts back into the question object ready for rendering in the template.
 			this.question.text = questiontext ? questiontext.innerHTML : "";
@@ -52,24 +52,24 @@ define("qtype_multichoiceset/mobile", ["core/str"], (str) => {
 				this.question.prompt = prompt.innerHTML;
 			}
 
-			var options = [];
-			var divs = answeroptions
+			const options = [];
+			const divs = answeroptions
 				? answeroptions.querySelectorAll("div[class^=r]")
 				: [];
 
 			Array.prototype.forEach.call(divs, (d) => {
 				// Each answer option contains all the data for presentation, it just needs extracting.
-				var checkbox = d.querySelector("input[type=checkbox]");
+				const checkbox = d.querySelector("input[type=checkbox]");
 				if (!checkbox) {
 					return;
 				}
 
-				var feedbackDiv = d.querySelector(
+				const feedbackDiv = d.querySelector(
 					"div.core-question-feedback-container",
 				);
-				var labelId = checkbox.getAttribute("aria-labelledby");
-				var labelElement = labelId
-					? d.querySelector("#" + labelId.replace(/:/g, "\\:"))
+				const labelId = checkbox.getAttribute("aria-labelledby");
+				let labelElement = labelId
+					? d.querySelector(`#${labelId.replace(/:/g, "\\:")}`)
 					: null;
 
 				// If not found, use the format used in older Moodle versions.
@@ -81,13 +81,13 @@ define("qtype_multichoiceset/mobile", ["core/str"], (str) => {
 					return;
 				}
 
-				var label = labelElement.innerHTML;
-				var name = checkbox.getAttribute("name");
-				var checked = checkbox.hasAttribute("checked");
-				var disabled = d.querySelector("input").hasAttribute("disabled");
-				var feedback = feedbackDiv ? feedbackDiv.innerHTML : "";
-				var qclass = d.getAttribute("class") || "";
-				var iscorrect = qclass.includes("core-question-answer-correct")
+				const label = labelElement.innerHTML;
+				const name = checkbox.getAttribute("name");
+				const checked = checkbox.hasAttribute("checked");
+				const disabled = d.querySelector("input").hasAttribute("disabled");
+				const feedback = feedbackDiv ? feedbackDiv.innerHTML : "";
+				const qclass = d.getAttribute("class") || "";
+				const iscorrect = qclass.includes("core-question-answer-correct")
 					? 1
 					: qclass.includes("core-question-answer-incorrect")
 						? 0
