ce2f78a9fe294ce4add233068def52b984b86c4b
Tim-Louis Rieck <timlouis.rieck@gmail.com>
2025-04-02 07:59:17 +0200

    old

--- mobile/multichoiceset.js
+++ mobile/multichoiceset.js
@@ -1,114 +1,86 @@
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+var that = this;
+var result = {
+	componentInit: function () {
+		// This.question should be provided to us here.
+		// This.question.html (string) is the main source of data, presumably prepared by the renderer.
+		// There are also other useful objects with question like infoHtml which is used by the
+		// page to display the question state, but with which we need do nothing.
+		// This code just prepares bits of this.question.html storing it in the question object ready for
+		// passing to the template (multichoiceset.html).
+		// Note this is written in 'standard' javascript rather than ES6. Both work.
 
-/**
- * JavaScript for the multichoiceset question type.
- *
- * @module     qtype_multichoiceset/mobile
- * @copyright  2024 Tim-Louis Rieck <tim-louis.rieck@oncampus.de>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-
-/**
- * Initialize the mobile component
- *
- * @return {boolean}
- */
-export const componentInit = function() {
-	if (!this.question) {
-		return this.CoreQuestionHelperProvider.showComponentError(this.onAbort);
-	}
-
-	// Create a temporary div to ease extraction of parts of the provided html.
-	const div = document.createElement('div');
-	div.innerHTML = this.question.html;
-
-	// Replace Moodle's correct/incorrect classes, feedback and icons with mobile versions.
-	this.CoreQuestionHelperProvider.replaceCorrectnessClasses(div);
-	this.CoreQuestionHelperProvider.replaceFeedbackClasses(div);
-
-	// Get useful parts of the provided question html data.
-	const questiontext = div.querySelector('.qtext');
-	const prompt = div.querySelector('.prompt');
-	const answeroptions = div.querySelector('.answer');
-
-	// Add the useful parts back into the question object ready for rendering in the template.
-	this.question.text = questiontext ? questiontext.innerHTML : '';
-
-	// Without the question text there is no point in proceeding.
-	if (!this.question.text) {
-		return this.CoreQuestionHelperProvider.showComponentError(this.onAbort);
-	}
-
-	if (prompt) {
-		this.question.prompt = prompt.innerHTML;
-	}
-
-	const options = [];
-	const divs = answeroptions
-		? answeroptions.querySelectorAll('div[class^=r]')
-		: [];
-
-	Array.prototype.forEach.call(divs, (d) => {
-		// Each answer option contains all the data for presentation, it just needs extracting.
-		const checkbox = d.querySelector('input[type=checkbox]');
-		if (!checkbox) {
-			return;
+		if (!this.question) {
+			return that.CoreQuestionHelperProvider.showComponentError(that.onAbort);
 		}
 
-		const feedbackDiv = d.querySelector(
-			'div.core-question-feedback-container'
-		);
-		const labelId = checkbox.getAttribute('aria-labelledby');
-		let labelElement = labelId
-			? d.querySelector(`#${labelId.replace(/:/g, '\\:')}`)
-			: null;
+		// Create a temporary div to ease extraction of parts of the provided html.
+		var div = document.createElement("div");
+		div.innerHTML = this.question.html;
 
-		// If not found, use the format used in older Moodle versions.
-		if (!labelElement) {
-			labelElement = d.querySelector('label');
+		// Replace Moodle's correct/incorrect classes, feedback and icons with mobile versions.
+		that.CoreQuestionHelperProvider.replaceCorrectnessClasses(div);
+		that.CoreQuestionHelperProvider.replaceFeedbackClasses(div);
+
+		// Get useful parts of the provided question html data.
+		var questiontext = div.querySelector(".qtext");
+		var prompt = div.querySelector(".prompt");
+		var answeroptions = div.querySelector(".answer");
+
+		// Add the useful parts back into the question object ready for rendering in the template.
+		this.question.text = questiontext.innerHTML;
+		// Without the question text there is no point in proceeding.
+		if (typeof this.question.text === "undefined") {
+			return that.CoreQuestionHelperProvider.showComponentError(that.onAbort);
+		}
+		if (prompt !== null) {
+			this.question.prompt = prompt.innerHTML;
 		}
 
-		if (!labelElement) {
-			return;
-		}
-
-		const label = labelElement.innerHTML;
-		const name = checkbox.getAttribute('name');
-		const checked = checkbox.hasAttribute('checked');
-		const disabled = d.querySelector('input').hasAttribute('disabled');
-		const feedback = feedbackDiv ? feedbackDiv.innerHTML : '';
-		const qclass = d.getAttribute('class') || '';
-		const iscorrect = qclass.includes('core-question-answer-correct')
-			? 1
-			: qclass.includes('core-question-answer-incorrect')
-				? 0
+		var options = [];
+		var divs = answeroptions.querySelectorAll("div[class^=r]"); // Only get the answer options divs (class="r0...").
+		divs.forEach(function (d, i) {
+			// Each answer option contains all the data for presentation, it just needs extracting.
+			var checkbox = d.querySelector("input[type=checkbox]");
+			var feedbackDiv = d.querySelector("div.core-question-feedback-container");
+			var labelId = checkbox.getAttribute("aria-labelledby");
+			var labelElement = labelId
+				? d.querySelector("#" + labelId.replace(/:/g, "\\:"))
 				: undefined;
+			if (!labelElement) {
+				// Not found, use the format used in older Moodle versions.
+				labelElement = d.querySelector("label");
+			}
 
-		options.push({
-			text: label,
-			name: name,
-			checked: checked,
-			disabled: disabled,
-			feedback: feedback,
-			qclass: qclass,
-			iscorrect: iscorrect,
+			var label = labelElement.innerHTML;
+			var name = checkbox.getAttribute("name");
+			var checked = checkbox.getAttribute("checked") ? true : false;
+			var disabled =
+				d.querySelector("input").getAttribute("disabled") === "disabled"
+					? true
+					: false;
+			var feedback = feedbackDiv ? feedbackDiv.innerHTML : "";
+			var qclass = d.getAttribute("class") || "";
+			var iscorrect =
+				qclass.indexOf("core-question-answer-correct") >= 0
+					? 1
+					: qclass.indexOf("core-question-answer-incorrect") >= 0
+						? 0
+						: undefined;
+			options.push({
+				text: label,
+				name: name,
+				checked: checked,
+				disabled: disabled,
+				feedback: feedback,
+				qclass: qclass,
+				iscorrect: iscorrect,
+			});
 		});
-	});
+		this.question.options = options;
 
-	this.question.options = options;
-	return true;
+		return true;
+	},
 };
+
+// This next line is required as is (because of an eval step that puts this result object into the global scope).
+result;
